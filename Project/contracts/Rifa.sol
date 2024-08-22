c6686dc7558d641fb176f37e1dea82b8c4192afd
Gustavo Pioli <gustavo_pioli@hotmail.com>
2024-08-22 13:30:27 -0300

    :sparkles: feat: Implementação do troco, agora o comprador será pago pelo valor extra enviado

--- Project/contracts/Rifa.sol
+++ Project/contracts/Rifa.sol
@@ -10,24 +10,32 @@ contract Rifa {
     uint256 public len;
 
     uint256 public maxEntradas;
+    uint256 public restEntradas; // Entradas restantes
+    uint256 public valorEntrada; // Preço de cada entrada
+    uint256 public numEntradas;
 
     constructor(uint256 _maxEntradas) {
         manager = msg.sender;
         sorteado = false;
         maxEntradas = _maxEntradas;
+        restEntradas = maxEntradas;
+        valorEntrada = .01 ether;
     }
 
     // Permite que usuários entrem na rifa com uma ou mais entradas
     function entrar() public payable {
         require(!sorteado, "O sorteio ja foi realizado");  // Verifica se o sorteio já foi realizado
-        require(msg.value > .01 ether, "Quantidade minima de ETH nao atingida");
+        require(msg.value > valorEntrada, "Quantidade minima de ETH nao atingida");
 
-        uint256 numEntradas = msg.value / .01 ether;
+        numEntradas = msg.value / valorEntrada;
+        verificaEntrada();
+        restEntradas = restEntradas - numEntradas;
+
         for (uint256 i = 0; i < numEntradas; i++) {
             entradas.push(msg.sender);
         }
 
-        premio += msg.value;
+        premio += numEntradas * valorEntrada;
         len = entradas.length;
 
         if(len >= maxEntradas){
@@ -35,7 +43,15 @@ contract Rifa {
         }
     }
 
+    function verificaEntrada() private {
+        if(numEntradas > restEntradas){
+            uint256 extraEntradas = numEntradas - restEntradas;
+            uint256 troco = extraEntradas * valorEntrada;
+            payable(msg.sender).transfer(troco);
 
+            numEntradas = restEntradas;
+        }
+    }
     // Função para gerar um número aleatório usando o hash do bloco
     function random() private view returns (uint256) {
         return uint256(keccak256(abi.encodePacked(block.prevrandao, block.timestamp, entradas)));
