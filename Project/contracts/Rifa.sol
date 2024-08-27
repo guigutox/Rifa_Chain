352965e27d09bdcf40b4bc8bcc39862e1675325c
brendorr <brendorg.br@ufu.br>
2024-08-27 09:34:56 -0300

    Hardhat funcionando localmente

--- Project/contracts/Rifa.sol
+++ Project/contracts/Rifa.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+import "./RealDigital.sol";  // Importa o contrato MyToken específico
+
 contract Rifa {
     address public manager;
     address[] public entradas;
@@ -9,50 +11,51 @@ contract Rifa {
     address public vencedor;
     uint256 public len;
 
-    uint256 public maxEntradas;
+    uint256 public maxEntradas;  // quantidade maxima de entradas
     uint256 public restEntradas; // Entradas restantes
     uint256 public valorEntrada; // Preço de cada entrada
-    uint256 public numEntradas;
+    uint256 public numEntradas;  // quantidade atual de entradas
 
+    RealDigital public token; // dizendo qual token erc20 vamos usar
 
-    constructor(uint256 _maxEntradas) {
+    constructor(RealDigital _token, uint256 _maxEntradas, uint256 _valorEntrada) {
         manager = msg.sender;
+        token = _token;
         sorteado = false;
         maxEntradas = _maxEntradas;
         restEntradas = maxEntradas;
-        valorEntrada = .01 ether;
+        valorEntrada = _valorEntrada;
     }
 
     // Permite que usuários entrem na rifa com uma ou mais entradas
-    function entrar() public payable {
-        require(!sorteado, "O sorteio ja foi realizado");  // Verifica se o sorteio já foi realizado
-        require(msg.value > valorEntrada, "Quantidade minima de ETH nao atingida");
+    function entrar(uint256 quantidadeTokens) public {
+        require(!sorteado, "O sorteio ja foi realizado");
+        require(quantidadeTokens >= valorEntrada, "Quantidade minima de tokens nao atingida");
 
-        numEntradas = msg.value / valorEntrada;
-        verificaEntrada();
-        restEntradas = restEntradas - numEntradas;
+        numEntradas = quantidadeTokens / valorEntrada;
+
+        // verifica se o sender nao tentou entrar com mais vagas do que existem
+        if (numEntradas > restEntradas) {
+            numEntradas = restEntradas;  // ajusta a quantidade de entradas que ele vai usar, tirando a necessidade de troco
+        }
+        // transfere a quantidade de tokens ja ajustada para o contrato
+        uint256 totalTransferencia = numEntradas * valorEntrada;
+        require(token.transferFrom(msg.sender, address(this), totalTransferencia), "Transferencia de tokens falhou");
 
         for (uint256 i = 0; i < numEntradas; i++) {
             entradas.push(msg.sender);
         }
 
-        premio += numEntradas * valorEntrada;
+        premio += totalTransferencia;
         len = entradas.length;
 
-        if(len >= maxEntradas){
+        restEntradas -= numEntradas;
+
+        if (len >= maxEntradas) {
             escolherVencedor();
         }
     }
 
-    function verificaEntrada() private {
-        if(numEntradas > restEntradas){
-            uint256 extraEntradas = numEntradas - restEntradas;
-            uint256 troco = extraEntradas * valorEntrada;
-            payable(msg.sender).transfer(troco);
-
-            numEntradas = restEntradas;
-        }
-    }
     // Função para gerar um número aleatório usando o hash do bloco
     function random() private view returns (uint256) {
         return uint256(keccak256(abi.encodePacked(block.prevrandao, block.timestamp, entradas)));
@@ -65,15 +68,12 @@ contract Rifa {
 
         uint256 index = random() % entradas.length;
         vencedor = entradas[index];
-        payable(vencedor).transfer(premio);
+
+        // Transfere os tokens acumulados como prêmio para o vencedor
+        require(token.transfer(vencedor, premio), "Transferencia do premio falhou");
+
         sorteado = true;
     }
-
-    // Modificador que garante que apenas o gerente possa executar certas funções
-  /*  modifier restricted() {
-        require(msg.sender == manager, "Somente o gerente pode chamar esta funcao");
-        ;
-    }*/
 
     // Função para visualizar os participantes da rifa
     function getEntradas() public view returns (address[] memory) {
@@ -83,8 +83,4 @@ contract Rifa {
     function getSorteado() public view returns (bool) {
         return sorteado;
     }
-
-
 }
-
-
---
+++ Project/ignition/modules/deployERC20.js
@@ -0,0 +1,31 @@
+async function main() {
+    const [deployer] = await ethers.getSigners();
+  
+    console.log("Deploying contracts with the account:", deployer.address);
+  
+    // Deploy RealDigital
+    const RealDigital = await ethers.getContractFactory("RealDigital");
+    const realDigital = await RealDigital.deploy();
+    await realDigital.waitForDeployment();  // importante aguardar esse negocio, se nao ele retorna undefined
+    const realDigitalAddress = await realDigital.getAddress();  // graças ao await ali, é garantia que o contrato ja vai ter feito o deploy e com o endereço em maos
+  
+    console.log("RealDigital implementado no endereço:", realDigitalAddress);
+  
+    // Deploy Rifa
+    const maxEntradas = 100;
+    const valorEntrada = ethers.parseUnits("10", 18); // 10 DREX por entrada
+    const Rifa = await ethers.getContractFactory("Rifa");
+    const rifa = await Rifa.deploy(realDigitalAddress, maxEntradas, valorEntrada);
+    await rifa.waitForDeployment();  // mesma coisa de ali em cima
+    const rifaAddress = await rifa.getAddress();  
+  
+    console.log("Rifa implementado no endereço:", rifaAddress);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+  
